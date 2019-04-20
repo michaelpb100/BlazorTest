513e24eb0a360379c3a588c29a043e94083d29b5
Michael Brooks <michaelbrooks100@yahoo.co.uk>
2019-04-20 18:15:33 +0100

    flex

---
+++ Bootstrap.Client/Shared/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootstrap.Client.Shared
+{
+    public class Class
+    {
+    }
+}
